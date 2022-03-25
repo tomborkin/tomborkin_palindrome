e7f470ee2bd96aeb381cdb8a3e36f3c4745e7da5
Tom Borkin <tomborkin@gmail.com>
2022-03-25 14:00:38 +1300

    Add new tests and punctuation handling

--- lib/tomborkin_palindrome.rb
+++ lib/tomborkin_palindrome.rb
@@ -2,7 +2,29 @@
 
 require_relative "tomborkin_palindrome/version"
 
-module TomborkinPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
-end
+
+  class String
+
+    # Returns true for a palindrome, false otherwise.
+    def palindrome?
+      processed_content == processed_content.reverse
+    end
+
+    # Returns the letters in the string.
+    def letters
+        the_letters = []
+        for i in 0..self.length - 1
+          if self[i].match(/[a-zA-Z]/)
+            the_letters << self[i]
+          end
+        end
+        the_letters.join
+      end
+
+    private
+
+      # Returns content for palindrome testing.
+      def processed_content
+        self.letters.downcase
+      end
+  end
